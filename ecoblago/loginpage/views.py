77f33484e48f5b9dd279d7824214be4a0a43b1b7
Yerassyl Zhassulanov <zhasulanov.erasyl@mail.ru>
2024-11-16 15:15:03 +0500

    Edit design of authentication page

--- ecoblago/loginpage/views.py
+++ ecoblago/loginpage/views.py
@@ -37,8 +37,7 @@ def login(request: HttpRequest) -> HttpResponse:
     }
     
     form = LoginForm()
-    errors = []
-    print(request.POST)
+    auth_errors = []
     if request.method.upper() == "POST":
         if "auth-option-signup" in request.POST:
             context["auth_option"] = 1
@@ -56,11 +55,11 @@ def login(request: HttpRequest) -> HttpResponse:
                     password = get_password_hash(form.cleaned_data["password"])
                     if len(User.objects.filter(phone=phone, password=password)) > 0:
                         return redirect("Here should be catalog, but i seems i didn't implemented it yet =(")
-                    print("Пользователь не найден")
-                    errors.append("Пользователь не найден")
+                    auth_errors.append("Пользователь не найден")
+
             else:
                 form = SignupForm(request.POST)
-                print(form)
+
                 if form.is_valid():
                     first_name = form.cleaned_data["first_name"]
                     last_name = form.cleaned_data["last_name"]
@@ -69,16 +68,25 @@ def login(request: HttpRequest) -> HttpResponse:
                     password = get_password_hash(form.cleaned_data["password"])
 
                     if len(User.objects.filter(phone=phone)) > 0:
-                        errors.append("Пользователь с таким номером телефона уже существует.")
+                        auth_errors.append("Пользователь с таким номером телефона уже существует.")
                     elif len(User.objects.filter(email=email)) > 0:
-                        errors.append("Пользователь с такой почтой уже существует.")
+                        auth_errors.append("Пользователь с такой почтой уже существует.")
                     else:
                         User.objects.create(first_name=first_name, last_name=last_name, phone=phone, email=email, password=password)
                         return redirect("Here should be catalog, but i seems i didn't implemented it yet =(")
 
     context["form"] = form
-    if len(errors) > 0:
-        context["errors"] = errors
+    if len(form.errors) > 0:
+        errors_data = form.errors.as_data()
+        error_data = errors_data[
+            list(
+                errors_data.keys()
+            )[0] # Рандомный ключ
+        ][0]  # берет лишь одну ошибку для красивого отображения
+        context["error_message"] = error_data.message
+
+    elif len(auth_errors) > 0:
+        context["error_message"] = auth_errors[0]
 
     current_auth_option = context["auth_option"]
     return render(request, "loginpage/loginpage.html", context=context)
