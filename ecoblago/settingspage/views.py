93316a116ca11ccea7e569bb0ff672185fb63425
Yerassyl Zhassulanov <zhasulanov.erasyl@mail.ru>
2025-01-27 19:01:54 +0500

    End the design of the settings page and add functionality of changing password

--- ecoblago/settingspage/views.py
+++ ecoblago/settingspage/views.py
@@ -3,10 +3,12 @@ from typing import Union
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, TemplateView
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse, Http404
 from django.middleware.csrf import get_token
 from django.contrib.auth.models import AbstractUser 
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from django.core.files.base import ContentFile
 from django.db.models import ImageField
 
@@ -15,8 +17,32 @@ from authpage.models import User
 class SettingspageView(TemplateView):
     template_name = "settingspage/settingspage.html"
 
-    def get(self, request) -> Union[HttpResponse, JsonResponse]:
+    def get(self, *args) -> Union[HttpResponse, JsonResponse]:
         return self.render_to_response(self.context)
+
+
+    def post(self, *args) -> Union[HttpResponse, JsonResponse]:
+        if "action" in self.request.POST:
+            return self.handle_ajax_post()
+        return Http404()
+    
+    def handle_ajax_post(self) -> JsonResponse:
+        if "change-password" == self.request.POST["action"]:
+            return self.change_password()
+        return Http404()
+
+    def change_password(self) -> JsonResponse:
+        new_password = self.request.POST["new_password"]
+        try:
+            validate_password(new_password)
+        except ValidationError as e:
+            return JsonResponse({"success": False, "error_message": e.messages[0]})
+
+        user = self.request.user
+        user.set_password(new_password)
+        user.save()
+
+        return JsonResponse({"success": True})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
